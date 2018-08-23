c6afd7390865c57d9a320eb2552977d67e201894
lishengsong <2353142548@qq.com>
2018-08-23 09:43:02 +0800

    仿知乎平台设计
    Signed-off-by: lishengsong <2353142548@qq.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,25 +6,66 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { DiscoveryPage } from '../pages/discovery/discovery';
+import { NotificationPage } from '../pages/notification/notification';
+import { ChatPage } from '../pages/chat/chat';
+import { MorePage } from '../pages/more/more';
+import { TabsPage } from '../pages/tabs/tabs';
+import { DetailsPage } from '../pages/details/details';
+import { RestProvider } from '../providers/rest/rest';
+import { IonicStorageModule } from '@ionic/storage';
+import { HttpModule } from '@angular/http';
+import { LoginPage } from '../pages/login/login';
+import { UserdatalistPage } from '../pages/userdatalist/userdatalist';
+import { ComponentsModule } from '../components/components.module';
+import { RegisterPage } from '../pages/register/register';
+import { UserPage } from '../pages/user/user';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    DetailsPage,
+    DiscoveryPage,
+    ChatPage,
+    NotificationPage,
+    MorePage,
+    TabsPage,
+    HomePage,
+    LoginPage,
+    UserPage,
+    RegisterPage,
+    UserdatalistPage,
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp, {
+      backButtonText: '返回',
+    }),
+    BrowserModule,
+    HttpModule, // 全局需要导入 HTTP
+    IonicStorageModule.forRoot(), // 全局定义 storage 的模块
+    ComponentsModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    DetailsPage,
+    DiscoveryPage,
+    ChatPage,
+    NotificationPage,
+    MorePage,
+    TabsPage,
+    HomePage,
+    LoginPage,
+    UserPage,
+    RegisterPage,
+    UserdatalistPage,
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    { provide: ErrorHandler, useClass: IonicErrorHandler },
+    RestProvider, // rest 的定义导入
   ]
 })
 export class AppModule {}
---
+++ src/components/components.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+/*import { IonicPageModule } from 'ionic-angular';*/
+/*import { EmojipickerComponent } from './emojipicker/emojipicker';*/
+import { QuestionListComponent } from './question-list/question-list';
+@NgModule({
+	declarations: [
+    QuestionListComponent],
+	imports: [],
+	exports: [
+    QuestionListComponent]
+})
+export class ComponentsModule {}
